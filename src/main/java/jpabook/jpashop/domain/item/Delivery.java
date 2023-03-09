16830a166e6c7615033275780b578be5c9968ea2
kjs <kjsaj0525@gmail.com>
2023-03-09 17:53:46 +0900

    fetch = lazy 설정

--- src/main/java/jpabook/jpashop/domain/item/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/item/Delivery.java
@@ -4,15 +4,18 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Entity
 @Getter@Setter
 public class Delivery {
 
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id@GeneratedValue(strategy = IDENTITY)
     @Column(name="delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery",fetch = LAZY)
     private Order order;
     @Embedded
     private Address address;
